b4279e146a60625fdb2e5aace551067c07b1c489
Raymond Walker <raymond.walker@greenpeace.org>
2020-08-14 14:35:04 +1000

    Switch to emitting the full extractJob dict in pubsub message.
    This way we're adherent to the Google Spec, supporting the emission of 
    multiple destination Uris.

--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ import base64
 import json
 import logging
 import sys
+import traceback
 
 from os import environ
 from time import sleep
 
-from google.cloud import storage
+# from google.cloud import storage
 from google.cloud import bigquery
 from google.cloud import pubsub_v1
 
@@ -23,87 +24,97 @@ RELEASE_STRING = "{}@{}".format(APP_NAME, APP_VERSION)
 
 sentry_sdk.init(dsn=environ["SENTRY_DSN"], release=RELEASE_STRING)
 
-LOGGER = logging.getLogger()
-
 BUCKET = environ["BUCKET"]
 ENTITY = environ["ENTITY"]
-ENVIRONMENT = environ["ENVIRONMENT"]
 
-if "prod" in ENVIRONMENT:
-    LOGGER.setLevel(logging.INFO)
+if "prod" in environ["ENVIRONMENT"]:
+    logging.basicConfig(
+        format="%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d - %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO
+    )
 else:
-    print("Debugging ...")
-    LOGGER.setLevel(logging.DEBUG)
+    logging.basicConfig(
+        format="%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d - %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG
+    )
+
+logging.info("%s: COLD", RELEASE_STRING)
 
 # Instantiate client
 BQ = bigquery.Client()
 
-LOGGER.info("%s: COLD", RELEASE_STRING)
 
-
-def get_dataset_ref(data):
+def get_dataset_ref(bq):
     """Return a Bigquery Dataset Reference"""
-    return bigquery.DatasetReference(data["project"], data["dataset"])
+    return bigquery.DatasetReference(bq["project"], bq["dataset"])
 
 
-def get_dataset_location(data):
+def get_dataset_location(bq):
     """Parse payload for dataset location"""
     # Default BQ location to EU, as that's where we prefer our datasets
     location = "EU"
     # ... but the payload knows best
-    if "location" in data:
-        location = data["location"]
+    if "location" in bq:
+        location = bq["location"]
     return location
 
 
-def get_destination_uri(data):
+def get_destination_object(bq):
     """Returns a parsed destination GCS URI"""
-    return "gs://{}/{}/{}.csv".format(BUCKET, data["dataset"], data["table"])
+    return "{}/{}.csv".format(bq["dataset"], bq["table"])
 
 
-def write_gcs_file(results, filename):
-    """Create a file.
-
-    The retry_params specified in the open call will override the default
-    retry params for this particular file handle.
-
-    Args:
-    filename: filename.
-    """
-    gcs = storage.Client()
-
-    bucket = gcs.bucket(BUCKET)
-    blob = bucket.blob(filename)
-
-    blob.upload_from_string(results)
-    LOGGER.info("Created")
+def get_destination_uri(bq):
+    """Returns a parsed destination GCS URI"""
+    return "gs://{}/{}".format(BUCKET, get_destination_object(bq))
 
 
-def bq_extract_table(data):
+# def write_gcs_file(results, filename):
+#     """Create a file.
+#
+#     The retry_params specified in the open call will override the default
+#     retry params for this particular file handle.
+#
+#     Args:
+#     filename: filename.
+#     """
+#     gcs = storage.Client()
+#
+#     bucket = gcs.bucket(BUCKET)
+#     blob = bucket.blob(filename)
+#
+#     blob.upload_from_string(results)
+#     logging.info("Created")
+
+
+# def bq_extract_view(bq):
+#     """Perform a manual query against the view, write to GCS blob"""
+#     query_job = BQ.query("""
+#     SELECT * FROM `{}.{}.{}`
+#     """.format(
+#         bq["project"],
+#         bq["dataset"],
+#         bq["view"]),
+#         location=get_dataset_location(bq)
+#     )
+#     results = query_job.result()  # Waits for job to complete.
+#     logging.debug(type(results))
+#     logging.info(results)
+#     write_gcs_file(results, get_destination_uri(bq))
+
+
+def bq_extract_table(bq):
     """Perform the BQ to GCS extract job"""
     extract_job = BQ.extract_table(
-        get_dataset_ref(data).table(data["table"]),
-        get_destination_uri(data),
+        get_dataset_ref(bq).table(bq["table"]),
+        get_destination_uri(bq),
         # Location must match that of the source table.
-        location=get_dataset_location(data),
+        location=get_dataset_location(bq),
     )
     results = extract_job.result()  # Waits for job to complete.
-    LOGGER.debug(type(results))
-    LOGGER.info(results)
-
-
-def bq_extract_view(data):
-    """Perform a manual query against the view, write to GCS blob"""
-    query_job = BQ.query("""
-    SELECT * FROM `{}.{}.{}`
-    """.format(data["project"],
-               data["dataset"],
-               data["view"]),
-                         location=get_dataset_location(data))
-    results = query_job.result()  # Waits for job to complete.
-    LOGGER.debug(type(results))
-    LOGGER.info(results)
-    write_gcs_file(results, get_destination_uri(data))
+    pub(results._properties) # <google.cloud.bigquery.job.ExtractJob>
 
 
 def get_callback(api_future, data, ref):
@@ -128,30 +139,38 @@ def get_callback(api_future, data, ref):
     return callback
 
 
-def pub(data):
+def pub(result):
     """Publishes a message to a Pub/Sub topic."""
 
     # Exit early if output topic is not set
     if not environ["OUTPUT_TOPIC"]:
+        logging.info("Skip PubSub, OUTPUT_TOPIC is blank")
         return 'ok'
+
+    info = "BigQuery extract complete in {}ms: {}.{}.{} => {}".format(
+        result["statistics"]["totalSlotMs"],
+        result["configuration"]["extract"]["sourceTable"]["projectId"],
+        result["configuration"]["extract"]["sourceTable"]["datasetId"],
+        result["configuration"]["extract"]["sourceTable"]["tableId"],
+        ",".join(result["configuration"]["extract"]["destinationUris"])
+    )
+
+    logging.info("%s", info)
+
+    message = json.dumps({
+        "entity": environ["ENTITY"],
+        "environment": environ["ENVIRONMENT"],
+        "event": "bq.extract.complete",
+        "info": info,
+        "result": result
+    })
+
+    # Keep track of the number of published messages.
+    ref = dict({"num_messages": 0})
 
     # Initialize a Publisher client.
     client = pubsub_v1.PublisherClient()
 
-    # Keep track of the number of published messages.
-    ref = dict({"num_messages": 0})
-    message = json.dumps({
-        "entity": environ["ENTITY"],
-        "component": "generic",
-        "environment": "test",
-        "event": "bq.table.updated",
-        "info": "Some helpful descriptive text",
-        "bq": {
-            "dataset": "samples",
-            "project": "bigquery-public-data",
-            "table": "shakespeare"
-        }
-    })
     # When you publish a message, the client returns a future.
     api_future = client.publish(
         environ["OUTPUT_TOPIC"],
@@ -167,21 +186,27 @@ def pub(data):
 
 
 def handler(event):
-    """Parses event payload, extracts data from BigQuery table, writes to GCS"""
+    """Parses event payload, extract data from BigQuery table, write to GCS"""
     json_data = base64.b64decode(event["data"]).decode("utf-8")
     data = json.loads(json_data)
 
-    if "table" in data:
-        bq_extract_table(data)
-        pub(data)
+    if "bq" not in data:
+        raise Exception("Invalid payload: no 'bq' field in payload", data)
+
+    if "table" in data["bq"]:
+        bq_extract_table(data["bq"])
         return 'ok'
 
-    if "view" in data:
-        bq_extract_view(data)
-        pub(data)
-        return 'ok'
+    if "view" in data["bq"]:
+        raise Exception("Extracts from BigQuery views not implemented.")
+        # bq_extract_view(data["bq"])
+        # pub(data["bq"])
+        # return 'ok'
 
-    raise Exception("Invalid payload: no 'view' or 'table' field in payload")
+    raise Exception(
+        "Invalid payload: no 'view' or 'table' field in payload",
+        data
+    )
 
 
 def main(event, context):
@@ -195,16 +220,17 @@ def main(event, context):
          `timestamp` field contains the publish time.
     """
     try:
-        LOGGER.debug("%s: HOT START", RELEASE_STRING)
+        logging.debug("%s: HOT START", RELEASE_STRING)
         handler(event)
-        sleep(1)
     except Exception:
         for error in sys.exc_info():
-            LOGGER.error("%s", error)
-        LOGGER.debug(event)
-        LOGGER.debug(context)
+            logging.error("%s", error)
+        traceback.print_exc(file=sys.stderr)
+        print(event)
+        print(context)
         sleep(5)
         raise
+
 
 # @todo: implement deadletter topic
 
@@ -220,9 +246,3 @@ def main(event, context):
 #     DESTINATION_PROJECT,
 #     APP_NAME,
 # )
-
-# project = "bigquery-public-data"
-# dataset_id = "samples"
-# table_id = "shakespeare"
-#
-# bucket_name = "gpi-data-test"
