006173450497c1ca424b4518027c8d1d56e88dad
Raymond Walker <raymond.walker@greenpeace.org>
2020-08-17 21:14:37 +1000

    Add topic outputs

--- tf/module/output.tf
+++ tf/module/output.tf
@@ -5,3 +5,11 @@ output "service_account_email" {
 output "bucket" {
   value = google_storage_bucket.destination.name
 }
+
+output "input_topic" {
+  value = var.input_topic
+}
+
+output "output_topic" {
+  value = var.output_topic
+}
